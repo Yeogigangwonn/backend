718d57f1a6e76b871952559b6130b12c1295689d
yunnjj72 <joyjung040@gmail.com>
2025-07-17 22:33:08 +0900

    fc

---
+++ src/main/groovy/com/app/yeogigangwon/YeogigangwonApplication.groovy
@@ -0,0 +1,13 @@
+package com.app.yeogigangwon
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class YeogigangwonApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(YeogigangwonApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/app/yeogigangwon/YeogigangwonApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.app.yeogigangwon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YeogigangwonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
