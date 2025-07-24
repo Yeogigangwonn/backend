c3499f7b8b32181ec52cdfd656179cdd283a31ec
Seonah <tjsghk8441@naver.com>
2025-07-24 12:58:21 +0900

    db연동 완료

--- src/main/java/com/app/yeogigangwon/domain/TourPlace.java
+++ src/main/java/com/app/yeogigangwon/domain/TourPlace.java
@@ -2,7 +2,11 @@ package com.app.yeogigangwon.domain;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Document(collection = "tour_places")
 public class TourPlace {
 
--- src/main/java/com/app/yeogigangwon/fetch/TourPlaceFetcher.java
+++ src/main/java/com/app/yeogigangwon/fetch/TourPlaceFetcher.java
@@ -18,7 +18,7 @@ public class TourPlaceFetcher {
                 + "serviceKey=" + apiKey
                 + "&MobileOS=ETC"
                 + "&MobileApp=test"
-                + "&_type=json" // xml -> json으로 받기
+                + "&_type=json"
                 + "&numOfRows=5"
                 + "&pageNo=1"
                 + "&areaCode=32";
@@ -26,15 +26,34 @@ public class TourPlaceFetcher {
         RestTemplate restTemplate = new RestTemplate();
 
         try {
-            Map response = restTemplate.getForObject(url, Map.class);
-            System.out.println("전체 응답 구조 확인:");
-            System.out.println(response);
+            Map<String, Object> response = restTemplate.getForObject(url, Map.class);
+
+            // JSON 응답 구조 따라 꺼내기
+            Map<String, Object> responseBody = (Map<String, Object>) ((Map) response.get("response")).get("body");
+            Map<String, Object> items = (Map<String, Object>) responseBody.get("items");
+            List<Map<String, Object>> itemList = (List<Map<String, Object>>) items.get("item");
+
+            List<TourPlace> tourPlaces = new ArrayList<>();
+
+            for (Map<String, Object> item : itemList) {
+                String id = String.valueOf(item.get("contentid"));
+                String name = (String) item.get("title");
+                String category = String.valueOf(item.get("contenttypeid"));
+                String location = (String) item.get("addr1");
+
+                TourPlace place = new TourPlace(id, name, category, location, 0); // 혼잡도는 기본값 0
+                tourPlaces.add(place);
+            }
+
+            return tourPlaces;
+
         } catch (Exception e) {
-            System.out.println("예외 발생:");
+            System.out.println("TourAPI 호출 또는 파싱 중 예외 발생:");
             e.printStackTrace();
         }
 
-        return Collections.emptyList(); // 일단 에러 방지용
+        return Collections.emptyList();
     }
+
 
 }
